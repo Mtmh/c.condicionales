86375b5592f3f4299cd9b4e0c330b273dd1f889c
Tiziano Martin hernando <tizianomartinhernando@MacBook-Pro-de-Tiziano.local>
2018-05-27 18:05:51 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int numero = 1;
+
+   if (numero == 0){
+       printf("Hola\n");
+   }
+
+   else
+   {
+       printf("Adios\n");
+   }
+
+    return 0;
+}
